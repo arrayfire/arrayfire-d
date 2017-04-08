c385b18df975b15e6b5dfa98a731cc3c1ea6deb7
prasun <prasunanand.bitsp@gmail.com>
2017-04-08 19:16:36 +0530

    added defines and version module

---
+++ source/arrayfire_d/af_version.d
@@ -0,0 +1,7 @@
+module arrayfire_d.af_version;
+
+const string VERSION = "3.4.0";
+const int AF_VERSION_MAJOR = 3;
+const int AF_VERSION_MINOR = 4;
+const int AF_VERSION_PATCH = 0;
+const int AF_API_VERSION_CURRENT = 34;
